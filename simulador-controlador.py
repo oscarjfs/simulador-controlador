ef8cf7942df63c6279a71c6b8102cf506d05f9c5
oscarjfs <oscarjfs@gmail.com>
2024-10-28 14:00:41 -0500

    Correcciones menores

--- simulador-controlador.py
+++ simulador-controlador.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
-import numpy as np
+from numpy import amax, amin
 from scipy.integrate import odeint
 from random import gauss
 from math import sqrt
@@ -8,36 +8,38 @@ from math import sqrt
 
 # parámetros del randomizador de señales
 mean = 1
-variance = 1e-5
+variance = 2e-8
 
 Ts = 0.1 # tiempo de muestreo
 controlAutomaticoEncendido = True
 
-Kp = 4.592591493136898 # ganancia del proceso
-taup = 15.142301049593728 # constante de tiempo del proceso
-td = 5.049971526325448 # tiempo muerto del proceso
+Kp = 4.5926 # ganancia del proceso
+taup = 15.1423 # constante de tiempo del proceso
+td = 5.0450 # tiempo muerto del proceso
 
-Kc = 0.6057530836821651 # ganancia proporcional del controlador
-Ki = 0.06063277987348371 # ganancia integral del controlador
-Kd = 0 # ganancia derivativa del controlador
+Kc = 0.6058 # ganancia proporcional del controlador
+Ki = 0.0606 # ganancia integral del controlador
+Kd = 0.0 # ganancia derivativa del controlador
 
-tfinal = 100
+tfinal = 200
 
 # condiciones inciales
 t0 = 0
-y0 = 0
-co0 = 0
+y0 = 50
+co0 = 50
 u0 = 0
-ysp0 = 0
+ysp0 = 50
 Ek2 = 0
 Ek1 = 0
 Ek = 0
 
-####### cambios en el sp
+####### cambios en escalón en el tiempo
 tstep1 = 5
-yspstep1 = 0.5
-tstep2 = 50
-yspstep2 = 1.5
+yspstep1 = 52
+coStep1 = 51
+tstep2 = 100
+yspstep2 = 50
+coStep2 = 50
 ########################
 
 tstep = tstep1
@@ -47,8 +49,7 @@ coActual = co0
 yspActual = ysp0
 
 ndatos = int(tfinal/Ts+1)
-t = [ti*Ts for ti in range(t0,ndatos)]
-
+t = [0]
 y = [y0]
 co = [co0]
 ysp = [ysp0]
@@ -62,27 +63,47 @@ def fopdt(y,t,co):
 
     return dydt
 
+# actualiza el valor de ysp desde el campo de entrada
+def actualizar_sp(yspNuevo):
+    global yspActual, tstep, co0, y0
+    yspActual = float(yspNuevo)
+    tstep = tActual
+    co0 = co[-1]
+    y0 = y[-1]
+
+# cambiar la salida del controlador cuando se ejecuta en manual
+def actualizar_co(coNuevo):
+    global coActual, tstep, co0, y0
+    coActual = float(coNuevo)
+    tstep = tActual
+    co0 = co[-1]
+    y0 = y[-1]
+
 # solución del modelo para cada paso o intervalo de tiempo (discretización)
 for i in range(0,ndatos-1):
-    ts = [t[i],t[i+1]]
 
-    tActual = t[i+1]
+    tActual = t[-1] + Ts
+    t.append(tActual)
+
+    ts = [t[-2],t[-1]]
+
     coActual = co[-1]
 
-    if tActual<tstep1:
-        ysp.append(ysp0)
-    elif tActual<tstep2:
-        ysp.append(yspstep1)
-    else:
-        ysp.append(yspstep2)
+    # perturbación en sp con controlador automático
+    # controlAutomaticoEncendido = True
+    # if tActual>=tstep1 and tActual<=tstep1+Ts:
+    #     actualizar_sp(yspstep1)
+    # elif tActual>=tstep2 and tActual<=tstep2+Ts:
+    #     actualizar_sp(yspstep2)
 
-    if ysp[-2] != ysp[-1]:
-        tstep = tActual
-        co0 = co[-1]
-        y0 = y[-1]
-
-    if (not controlAutomaticoEncendido) and (co[-2] != co[-1]):
-        tstep = tActual
+    # perturbación en el elemento final de control con controlador en manual
+    controlAutomaticoEncendido = False
+    if tActual>=tstep1 and tActual<=tstep1+Ts:
+        actualizar_co(coStep1)
+    elif tActual>=tstep2 and tActual<=tstep2+Ts:
+        actualizar_co(coStep2)
+    
+    ysp.append(yspActual)
 
     y1 = odeint(fopdt,y[-1],ts,args=tuple([co[-1]]))
     y.append(float(y1[-1])*gauss(mean,sqrt(variance)))
@@ -93,7 +114,12 @@ for i in range(0,ndatos-1):
         Ek1 = Ek
         Ek = ysp[-1] - y[-1]
         deltaCO = (Kc+Ki*Ts+Kd/Ts)*Ek - (Kc+2*Kd/Ts)*Ek1 + (Kd/Ts)*Ek2
-        co.append(co[-1] + deltaCO)
+        if co[-1] + deltaCO < 0:
+                co.append(0)
+        if co[-1] + deltaCO > 100:
+            co.append(100)           
+        else:
+            co.append(co[-1] + deltaCO)
     else:
         co.append(coActual)
 
@@ -103,7 +129,6 @@ fig, ax = plt.subplots(figsize=(13,8),facecolor='grey') # creación de figura
 plt.title("Gráfica de Tendencia",color='black',size=16, family="Arial") # asigna el título de la gráfica
 
 plt.xlim(0, tfinal) # ajusta el rango del eje x
-plt.ylim(round(np.amin(y)*.9), round(np.amax(y))*1.1 if np.amax(y)>np.amax(ysp) else round(np.amax(ysp))*1.1) # ajusta el rango del eje y
 ax.set_facecolor('black') # asignación del fondo de la gráfica
 
 ax.axhline(linewidth=2, color='w') # ajuste de propiedades del eje x
@@ -118,9 +143,14 @@ ax.tick_params(direction='out', colors='w', grid_color='w', grid_alpha=0.3) # pa
 twax = ax.twinx() # creación del eje y secundario
 twax.set_ylabel('CO',color='purple')
 twax.tick_params(direction='out', length=6, width=1, colors='purple', grid_color='w', grid_alpha=0.5) # parámetros de las marcaciones de los ejes
-twax.set_ylim(0, 1 if np.amax(co)<1 else round(np.amax(co))*1.1)
 
 ############# CARGUE DE DATOS
+
+ax.set_ylim(amin([round(amin(y)), round(amin(ysp))])*.95,
+            amax([round(amax(y)), round(amax(ysp)), 1])*1.05) # ajusta el rango del eje y principal
+twax.set_ylim(0 if amin(co)<0 else round(amin(co))*0.95,
+        1 if amax(co)<1 else round(amax(co))*1.05) # ajusta el rango del eje y secundario
+
 
 lineCO, = twax.plot(t, co, color ='purple', linestyle='solid') # crea la línea con los datos
 lineSP, = ax.plot(t, ysp, color ='r', linestyle='solid') # crea la línea con los datos
